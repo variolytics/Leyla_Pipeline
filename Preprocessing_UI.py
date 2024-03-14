634e747a0696adeda863aa2442916ff0b923e7db
Leyla <info@variolytics.com>
2024-03-14 18:57:28 +0100

    filter function added

--- Preprocessing_UI.py
+++ Preprocessing_UI.py
@@ -1,42 +1,27 @@
 import os
-import csv
+import pandas as pd
 import matplotlib.pyplot as plt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QTextEdit, QListWidget
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QListWidget
+from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QSize
 from Preprocessing_UI_Layout import Ui_winMain
-import pandas as pd
-
-
 
 class MyMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
-        # Setup UI from the generated class
         self.ui = Ui_winMain()
         self.ui.setupUi(self)
-
-        # Load icons
         self.load_icons()
-
-        # Connect signals to slots
         self.connect_signals()
-
-         # Enable multi-selection mode for the list widget
         self.ui.SelectedFile_listWidget.setSelectionMode(QListWidget.MultiSelection)
 
-        
-
     def load_icons(self):
-        # Get the path to the icons directory relative to the current file
         icons_dir = os.path.join(os.path.dirname(__file__), 'icons')
-        # Define icon paths and their corresponding widget attributes
         icon_paths = {
             'ScreeningFile': 'Data_Visualization.png',
-            'filterButton' : 'Filter_Button.png',
-            'addButton' : 'Add_Button.png',
-            'removeButton' : 'Remove_Button.png',
+            'filterButton': 'Filter_Button.png',
+            'addButton': 'Add_Button.png',
+            'removeButton': 'Remove_Button.png',
         }
         tab_icon_paths = {
             0: 'main_screening.png',
@@ -44,93 +29,165 @@ class MyMainWindow(QMainWindow):
             2: 'main_setting.png',
             3: 'main_help.png'
         }
-        # Load icons for individual widgets
         for widget_name, icon_name in icon_paths.items():
             icon_path = os.path.join(icons_dir, icon_name)
             icon = QIcon(icon_path)
             getattr(self.ui, widget_name).setIcon(icon)
-            # Set the icon size directly
-            getattr(self.ui, widget_name).setIconSize(QSize(40, 40))  # Adjust the width and height as needed
-        # Load icons for tab icons
+            getattr(self.ui, widget_name).setIconSize(QSize(40, 40))  
         for tab_index, icon_name in tab_icon_paths.items():
             icon_path = os.path.join(icons_dir, icon_name)
             self.ui.tbwMain.setTabIcon(tab_index, QIcon(icon_path))
-        # Set application icon
         app_icon_path = os.path.join(icons_dir, 'app_icon.png')
         self.setWindowIcon(QIcon(app_icon_path))
 
     def connect_signals(self):
-        # Connect signals to slots
-        self.ui.ScreeningFile.clicked.connect(self.visualizeFile)
-        self.ui.addButton.clicked.connect(self.openFileDialog)
-        self.ui.removeButton.clicked.connect(self.removeSelectedFiles)
-        
+        self.ui.ScreeningFile.clicked.connect(self.visualize_file)
+        self.ui.addButton.clicked.connect(self.open_file_dialog)
+        self.ui.removeButton.clicked.connect(self.remove_selected_files)
+        self.ui.filterButton.clicked.connect(self.save_last_100_data_for_each_file)
 
-    def openFileDialog(self):
+    def open_file_dialog(self):
         file_dialog = QFileDialog(self)
         file_dialog.setNameFilter("CSV files (*.csv)")
         file_dialog.setViewMode(QFileDialog.Detail)
         filenames, _ = file_dialog.getOpenFileNames(self, "Open CSV Files", "", "CSV files (*.csv)")
         if filenames:
-                # Add selected file names to the text edit
             self.ui.SelectedFile_listWidget.addItems(filenames)
-        self.updateRemoveButtonState()   
-    
+        self.update_remove_button_state()
 
-    def removeSelectedFiles(self):
-        # Get the currently selected items
+    def remove_selected_files(self):
         selected_items = self.ui.SelectedFile_listWidget.selectedItems()
         for item in selected_items:
             self.ui.SelectedFile_listWidget.takeItem(self.ui.SelectedFile_listWidget.row(item))
-        self.updateRemoveButtonState()
+        self.update_remove_button_state()
 
-
-    def updateRemoveButtonState(self):
+    def update_remove_button_state(self):
         selected_items_count = self.ui.SelectedFile_listWidget.count()
         self.ui.removeButton.setEnabled(selected_items_count > 0)
 
-    def visualizeFile(self):
+    def visualize_file(self):
         file_dialog = QFileDialog(self)
         file_dialog.setNameFilter("CSV files (*.csv)")
         file_dialog.setViewMode(QFileDialog.Detail)
         filename, _ = file_dialog.getOpenFileName(self, "Open CSV File", "", "CSV files (*.csv)")
         if filename:
             data = self.read_csv(filename)
-            if data:
+            if data is not None and not data.empty:  
                 self.ui.figure.clear()
                 ax = self.ui.figure.add_subplot(111)
-                ax.scatter(data["Index"], data["15.0"])
-                ax.set_xlabel("Index", labelpad=15, fontsize=12)
-                ax.set_ylabel("15.0", fontsize=12)
-
+                if "Methan" in filename:
+                    ax.scatter(data.index, data["15.0"])
+                    ax.set_xlabel("Index", labelpad=15, fontsize=12)
+                    ax.set_ylabel("15.0", fontsize=12)
+                elif "Lachgas" in filename:
+                    ax.scatter(data.index, data["44.0"])
+                    ax.set_xlabel("Index", labelpad=15, fontsize=12)
+                    ax.set_ylabel("44.0", fontsize=12)
                 plt.subplots_adjust(bottom=0.25)
                 self.ui.canvas.draw()
 
     def read_csv(self, filename):
         try:
-            # Read CSV file using pandas
             df = pd.read_csv(filename, sep=';', decimal=',')
-    
-            # Extract required columns
-            data = {
-                "Index": df.index.tolist(),  # Use the DataFrame index as x-axis values
-                "15.0": df["15.0"].astype(float).tolist()
-            }
-    
-            # Set plot label
+            if "Methan" in filename:
+                data = df[["15.0", "19.0","40.0","44.0"]]
+            elif "Lachgas" in filename:
+                data = df[["30.0", "19.0", "40.0", "44.0"]]
             filename_without_path = os.path.basename(filename)
             self.ui.Plot_label.setText(f"Preview: {filename_without_path}")
-    
             return data
         except Exception as e:
             print("Error reading CSV:", e)
             return None
 
+    def filter_data(self):
+        try:
+            last_100_data = []
+            file_names = [self.ui.SelectedFile_listWidget.item(index).text() for index in range(self.ui.SelectedFile_listWidget.count())]
+            for filename in file_names:
+                data = self.read_csv(filename)
+                if data is not None:
+                    last_100_data.append(data.iloc[-100:, :])
+            return last_100_data
+        except Exception as e:
+            print("Error reading CSV:", e)
+            return None
+
+    def save_last_100_data(self, data_list, file_prefix):
+        try:
+            script_directory = os.path.dirname(__file__)
+            output_directory = os.path.join(script_directory, 'last_100_data')
+
+            # Create the directory if it doesn't exist
+            if not os.path.exists(output_directory):
+                os.makedirs(output_directory)
+
+            for i, data in enumerate(data_list):
+                filename = f"{file_prefix.rstrip('_last_100.csv')}_last_100_{i+1}.csv"
+                output_file = os.path.join(output_directory, filename)
+                data.to_csv(output_file, index=False)
+                    
+        except Exception as e:
+            print("Error saving CSV:", e)
+
+
+
+    def concatenate_csv(self):
+        try:
+            # Get all CSV files with '_last_100.csv' in their names
+            script_directory = os.path.dirname(__file__)
+            output_directory = os.path.join(script_directory, 'last_100_data')
+            temperature_list = [f"{temp}°C" for temp in {10,12,15,20}]
+            for temperature in temperature_list:
+                csv_files = [file for file in os.listdir(output_directory) if file.endswith('.csv') and temperature in file]
+                # Concatenate rows from all CSV files
+                concatenated_df = pd.DataFrame()  # Initialize an empty DataFrame
+                for file in csv_files:
+                    df = pd.read_csv(os.path.join(output_directory, file))
+                    df = df.reset_index(drop=True)  # Reset index
+                    concatenated_df = pd.concat([concatenated_df, df], axis=0, ignore_index=True)
+                if "Lachgas0,5%" in file:
+                    concatenated_filename = os.path.join(output_directory, f"Lachgas0,5%_{temperature}_Concatenated.csv")
+                    concatenated_df.to_csv(concatenated_filename, index=False)
+                elif "Lachgas1%" in file:
+                    concatenated_filename = os.path.join(output_directory, f"Lachgas1%_{temperature}_Concatenated.csv")
+                    concatenated_df.to_csv(concatenated_filename, index=False)
+                elif "Methan0,1%" in file:
+                    concatenated_filename = os.path.join(output_directory, f"Methan0,1%_{temperature}_Concatenated.csv")
+                    concatenated_df.to_csv(concatenated_filename, index=False)
+                elif "Methan0,5%" in file:
+                    concatenated_filename = os.path.join(output_directory, f"Methan0,5%_{temperature}_Concatenated.csv")
+                    concatenated_df.to_csv(concatenated_filename, index=False)
+                elif "Methan1%" in file:
+                    concatenated_filename = os.path.join(output_directory, f"Methan1%_{temperature}_Concatenated.csv")
+                    concatenated_df.to_csv(concatenated_filename, index=False)
+
+        except Exception as e:
+            print("Error concatenating CSV files:", e)
+
+
+    def save_last_100_data_for_each_file(self):
+        try:
+            last_100_data = self.filter_data()
+            if last_100_data is not None:
+                output_directory = os.path.join(os.path.dirname(__file__), 'last_100_data')
+                for index, data in enumerate(last_100_data):
+                    output_filename = self.ui.SelectedFile_listWidget.item(index).text() 
+                    file_name_without_extension, file_extension = os.path.splitext(output_filename)
+                    output_filename = os.path.basename(file_name_without_extension)+"_last_100.csv"
+                    output_file = os.path.join(output_directory, output_filename)
+                    self.save_last_100_data([data], output_file)
+                    self.ui.FilteredFile_listWidget.addItems([output_filename])
+            self.concatenate_csv()
+        except Exception as e:
+            print("Error saving last 100 data:", e)
+
+
+    
 
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
     window = MyMainWindow()
-    window.setWindowIcon(QIcon(r'C:\Users\Leyla Roohisefat\DataVisualization\src\icons\app_icon.ico'))
     window.show()
     sys.exit(app.exec_())
--- Preprocessing_UI_Layout.py
+++ Preprocessing_UI_Layout.py
@@ -1,37 +1,25 @@
 import sys
-
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import (
-    QApplication,
-    QMainWindow,
-    QVBoxLayout,
-    QWidget,
-    QPushButton,
-    QFileDialog,
-    QAction,
-)
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
-
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-
-
 
 class Ui_winMain(object):
     def setupUi(self, winMain):
         winMain.setObjectName("winMain")
         winMain.resize(928, 722)
         winMain.setFixedSize(winMain.size()) 
+
         self.centralwidget = QtWidgets.QWidget(winMain)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName("gridLayout")
+
         self.tbwMain = QtWidgets.QTabWidget(self.centralwidget)
         self.tbwMain.setEnabled(True)
         self.tbwMain.setIconSize(QtCore.QSize(50, 50))
         self.tbwMain.setObjectName("tbwMain")
+
         self.wDataScreening = QtWidgets.QWidget()
         self.wDataScreening.setEnabled(True)
         self.wDataScreening.setObjectName("wDataScreening")
@@ -39,55 +27,55 @@ class Ui_winMain(object):
         self.gridLayout_8.setObjectName("gridLayout_8")
         self.verticalLayout_2 = QtWidgets.QVBoxLayout()
         self.verticalLayout_2.setObjectName("verticalLayout_2")
+
         self.scrollArea = QtWidgets.QScrollArea(self.wDataScreening)
         self.scrollArea.setWidgetResizable(True)
         self.scrollArea.setObjectName("scrollArea")
         self.scrollAreaWidgetContents = QtWidgets.QWidget()
         self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 884, 574))
         self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+
         self.horizontalLayoutWidget = QtWidgets.QWidget(self.scrollAreaWidgetContents)
         self.horizontalLayoutWidget.setGeometry(QtCore.QRect(711, 500, 171, 41))
         self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
         self.horizontalLayout_3 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
         self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+
         self.ScreeningFile = QtWidgets.QPushButton(self.horizontalLayoutWidget)
         self.ScreeningFile.setObjectName("ScreeningFile")
         self.horizontalLayout_3.addWidget(self.ScreeningFile)
-        # Increase the font and button size of the ScreeningFile button
-        button_font = QtGui.QFont()
-        button_font.setPointSize(12)  # Adjust the font size as needed
-        button_font.setBold(True)  # Make the font bold
-        self.ScreeningFile.setFont(button_font)
 
-        # Set size policy for the button
-        self.ScreeningFile.setSizePolicy(
-            QtWidgets.QSizePolicy.Fixed,
-            QtWidgets.QSizePolicy.Fixed
-        )
-        # Increase the size of the push button
-        self.ScreeningFile.setFixedSize(150, 40)  # Adjust the width and height as needed
-        
+        button_font = QtGui.QFont()
+        button_font.setPointSize(12)
+        button_font.setBold(True)
+        self.ScreeningFile.setFont(button_font)
+        self.ScreeningFile.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        self.ScreeningFile.setFixedSize(150, 40)
+
         self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.scrollAreaWidgetContents)
         self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(10, 0, 871, 491))
         self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
         self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
         self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout_4.setObjectName("verticalLayout_4")
+
         self.Plot_label = QtWidgets.QLabel(self.verticalLayoutWidget_2)
         self.Plot_label.setObjectName("Plot_label")
         self.verticalLayout_4.addWidget(self.Plot_label)
-        # Set font size for the "Data Preview" label
+
         font = QtGui.QFont()
-        font.setPointSize(12)  # Adjust the font size as needed
-        font.setBold(True)  # Make the font bold
+        font.setPointSize(12)
+        font.setBold(True)
         self.Plot_label.setFont(font)
+
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
         self.verticalLayout_2.addWidget(self.scrollArea)
         self.gridLayout_8.addLayout(self.verticalLayout_2, 0, 0, 1, 2)
+
         icon = QtGui.QIcon()
         self.tbwMain.addTab(self.wDataScreening, icon, "")
-        
+
         self.wFiltering = QtWidgets.QWidget()
         self.wFiltering.setEnabled(True)
         self.wFiltering.setObjectName("wFiltering")
@@ -97,141 +85,102 @@ class Ui_winMain(object):
         self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setObjectName("verticalLayout")
+
         spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.verticalLayout.addItem(spacerItem)
-        # Add a label above the QListWidget
+
         self.labelAboveListWidget = QtWidgets.QLabel(self.verticalLayoutWidget)
         self.labelAboveListWidget.setObjectName("labelAboveListWidget")
         self.labelAboveListWidget.setText("Selected Files:")
-        # Set font size for the label (adjust as needed)
+
         label_font = QtGui.QFont()
         label_font.setPointSize(12)
         label_font.setBold(True)
         self.labelAboveListWidget.setFont(label_font)
-
-        # Set size policy for the label
-        self.labelAboveListWidget.setSizePolicy(
-            QtWidgets.QSizePolicy.Expanding,
-            QtWidgets.QSizePolicy.Fixed
-        )
-
+        self.labelAboveListWidget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
         self.verticalLayout.addWidget(self.labelAboveListWidget)
-       
+
         self.SelectedFile_listWidget = QtWidgets.QListWidget(self.verticalLayoutWidget)
         self.SelectedFile_listWidget.setObjectName("listWidget")
-        # Set size policy for the QListWidget
-        self.SelectedFile_listWidget.setSizePolicy(
-            QtWidgets.QSizePolicy.Expanding,
-            QtWidgets.QSizePolicy.Expanding
-        )
+        self.SelectedFile_listWidget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         self.verticalLayout.addWidget(self.SelectedFile_listWidget)
-        
+
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
         spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem1)
+
         self.seletFile_label = QtWidgets.QLabel(self.verticalLayoutWidget)
         self.seletFile_label.setObjectName("seletFile_label")
         self.horizontalLayout.addWidget(self.seletFile_label)
+
         seletFile_label_font = QtGui.QFont()
         seletFile_label_font.setPointSize(12)
         seletFile_label_font.setBold(True)
         self.seletFile_label.setFont(seletFile_label_font)
+        self.seletFile_label.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
 
-        # Set size policy for the label
-        self.seletFile_label.setSizePolicy(
-            QtWidgets.QSizePolicy.Expanding,
-            QtWidgets.QSizePolicy.Fixed
-        )
         self.addButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
         self.addButton.setEnabled(True)
         self.addButton.setObjectName("addButton")
         self.horizontalLayout.addWidget(self.addButton)
+
         addButton_font = QtGui.QFont()
         addButton_font.setPointSize(12)
         addButton_font.setBold(True)
         self.addButton.setFont(addButton_font)
+        self.addButton.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
 
-        # Set size policy for the label
-        self.addButton.setSizePolicy(
-            QtWidgets.QSizePolicy.Expanding,
-            QtWidgets.QSizePolicy.Fixed
-        )
         self.removeButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
         self.removeButton.setObjectName("removeButton")
         self.horizontalLayout.addWidget(self.removeButton)
+
         removeButton_font = QtGui.QFont()
         removeButton_font.setPointSize(12)
         removeButton_font.setBold(True)
         self.removeButton.setFont(removeButton_font)
+        self.removeButton.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
 
-        # Set size policy for the label
-        self.removeButton.setSizePolicy(
-            QtWidgets.QSizePolicy.Expanding,
-            QtWidgets.QSizePolicy.Fixed
-        )
         self.verticalLayout.addLayout(self.horizontalLayout)
-        
-        
-        # Add a label above the second QListWidget
+
         self.labelAboveListWidget2 = QtWidgets.QLabel(self.verticalLayoutWidget)
         self.labelAboveListWidget2.setObjectName("labelAboveListWidget2")
         self.labelAboveListWidget2.setText("Filtered Files:")
-        # Set font size for the label (adjust as needed)
+        
         label_font = QtGui.QFont()
         label_font.setPointSize(12)
         label_font.setBold(True)
         self.labelAboveListWidget2.setFont(label_font)
-
-        # Set size policy for the label
-        self.labelAboveListWidget2.setSizePolicy(
-            QtWidgets.QSizePolicy.Expanding,
-            QtWidgets.QSizePolicy.Fixed
-        )
+        self.labelAboveListWidget2.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
 
         self.verticalLayout.addWidget(self.labelAboveListWidget2)
-       
-        # Add a second QListWidget for filtered files
+
         self.FilteredFile_listWidget = QtWidgets.QListWidget(self.verticalLayoutWidget)
         self.FilteredFile_listWidget.setObjectName("FilteredFile_listWidget")
-        # Set size policy for the second QListWidget
-        self.FilteredFile_listWidget.setSizePolicy(
-            QtWidgets.QSizePolicy.Expanding,
-            QtWidgets.QSizePolicy.Expanding
-        )
+        self.FilteredFile_listWidget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         self.verticalLayout.addWidget(self.FilteredFile_listWidget)
-        
-        # Create a horizontal layout for the Filter button
+
         self.horizontalLayoutFilter = QtWidgets.QHBoxLayout()
         self.horizontalLayoutFilter.setObjectName("horizontalLayoutFilter")
         spacerItemFilter = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayoutFilter.addItem(spacerItemFilter)
-        
-        # Add the Filter button to the horizontal layout
+
         self.filterButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
         self.filterButton.setObjectName("filterButton")
         self.filterButton.setText("Filter")
+        
         filterButton_font = QtGui.QFont()
         filterButton_font.setPointSize(12)
         filterButton_font.setBold(True)
         self.filterButton.setFont(filterButton_font)
-        
-        # Set size policy for the Filter button
-        self.filterButton.setSizePolicy(
-            QtWidgets.QSizePolicy.Fixed,  # Use Fixed size policy to prevent expanding
-            QtWidgets.QSizePolicy.Fixed
-        )
-        
-        # Add the Filter button to the horizontal layout
+        self.filterButton.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+
         self.horizontalLayoutFilter.addWidget(self.filterButton)
-        
-        # Add the horizontal layout to the main vertical layout
         self.verticalLayout.addLayout(self.horizontalLayoutFilter)
-        
-        
-        
+
         icon1 = QtGui.QIcon()
         self.tbwMain.addTab(self.wFiltering, icon1, "")
+
         self.wSetting = QtWidgets.QWidget()
         self.wSetting.setObjectName("wSetting")
         self.horizontalLayoutWidget_2 = QtWidgets.QWidget(self.wSetting)
@@ -242,6 +191,7 @@ class Ui_winMain(object):
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         icon2 = QtGui.QIcon()
         self.tbwMain.addTab(self.wSetting, icon2, "")
+
         self.wHelp = QtWidgets.QWidget()
         self.wHelp.setObjectName("wHelp")
         self.gridLayout_6 = QtWidgets.QGridLayout(self.wHelp)
@@ -258,31 +208,30 @@ class Ui_winMain(object):
         self.gridLayout_6.addWidget(self.tbwHelp, 0, 0, 1, 1)
         icon3 = QtGui.QIcon()
         self.tbwMain.addTab(self.wHelp, icon3, "")
+
         self.gridLayout.addWidget(self.tbwMain, 0, 1, 1, 1)
         winMain.setCentralWidget(self.centralwidget)
+
         self.mbMain = QtWidgets.QMenuBar(winMain)
         self.mbMain.setGeometry(QtCore.QRect(0, 0, 928, 22))
         self.mbMain.setObjectName("mbMain")
         winMain.setMenuBar(self.mbMain)
+
         self.stbStatusbar = QtWidgets.QStatusBar(winMain)
         self.stbStatusbar.setObjectName("stbStatusbar")
         winMain.setStatusBar(self.stbStatusbar)
+
         self.figure = plt.figure()
         self.canvas = FigureCanvas(self.figure)
-        # Increase the size of the canvas
-        self.canvas.setSizePolicy(
-            QtWidgets.QSizePolicy.Expanding,
-            QtWidgets.QSizePolicy.Expanding
-        )
+        self.canvas.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         self.verticalLayout_4.addWidget(self.canvas)
-        # Add a toolbar for zoom, pan, and home
+
         self.toolbar = NavigationToolbar(self.canvas)
         self.verticalLayout_4.addWidget(self.toolbar)
 
-        # Set font size and minimum width for the tabs
         tab_style = "QTabBar::tab { font-size: 11pt; min-width: 150px; }"
         self.tbwMain.setStyleSheet(tab_style)  
-        
+
         self.retranslateUi(winMain)
         self.tbwMain.setCurrentIndex(0)
         self.tbwHelp.setCurrentIndex(1)
@@ -293,9 +242,10 @@ class Ui_winMain(object):
     def retranslateUi(self, winMain):
         _translate = QtCore.QCoreApplication.translate
         winMain.setWindowTitle(_translate("winMain", "Preprocessing Tool"))
-        # Create a bold font
+
         bold_font_Tab = QtGui.QFont()
         bold_font_Tab.setBold(True)
+
         self.ScreeningFile.setText(_translate("winMain", "Select File"))
         self.removeButton.setText(_translate("winMain", "Remove"))
         self.addButton.setText(_translate("winMain", "Add"))
@@ -312,9 +262,7 @@ class Ui_winMain(object):
         self.tbwMain.setTabText(self.tbwMain.indexOf(self.wHelp), _translate("winMain", "Help"))
         self.tbwMain.tabBar().setFont(bold_font_Tab)
 
-
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
     winMain = QtWidgets.QMainWindow()
     ui = Ui_winMain()
